0530b754d5f43fc533c6a9d0bc589435a7f3a453
Deirdre18 <deirdreweldon18@gmail.com>
2020-07-14 16:36:39 +0100

    Re-comming respository previous committed under different name

---
+++ README.md
@@ -0,0 +1,3 @@
+# cordovaReactAPOD
+
+Referred to this link for guidance - https://medium.com/@pshubham/using-react-with-cordova-f235de698cc3
---
+++ platforms/android/app/src/main/assets/www/precache-manifest.b02a16a426859e81798de467bcddb3da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeacf484bc50921f1b5f6469976d7241",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59670e9d835869199569",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f65c793e11345bfab6df",
+    "url": "./static/js/2.6d994401.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6d994401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59670e9d835869199569",
+    "url": "./static/js/main.9dfee538.chunk.js"
+  },
+  {
+    "revision": "ce99b3157e5ae6527d3d",
+    "url": "./static/js/runtime-main.2b72dc68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
